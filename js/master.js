bc0bcf5641508c8d1736cf58eeae26eea9bf46cc
Kate Willison <kt.willison@gmail.com>
2016-03-07 19:40:33 -0800

    Added tripBoard html

--- js/master.js
+++
@@ -1,8 +0,0 @@
-$('.photo-tile').click(function(){
-	$("#photo-details-photo").attr('src', $($(this)[0]).attr("src"));
-	$(".photo-details").removeClass('photo-details-hidden');
-});
-
-$(".photo-details-close").click(function(){
-	$('.photo-details').addClass('photo-details-hidden');
-});
